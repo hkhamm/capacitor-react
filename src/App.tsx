28f3908c426c1dedf960a92b10742a02b3f57914
Keith Hamm <hkhamm@gmail.com>
2019-10-15 11:00:12 -0700

    adds reload line back to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,9 @@ const App: FC = () => {
         <div className={classes.app}>
             <header className={classes.appHeader}>
                 <img src={logo} className={classes.appLogo} alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
                 <a className={classes.appLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a>
